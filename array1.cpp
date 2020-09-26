d552b3d70075dea76c5ceb88436f3423e5454dd4
bhavinisehgal18 <62087213+bhavinisehgal18@users.noreply.github.com>
2020-09-27 00:46:13 +0530

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cin>>n;
+    cout<<"Array is:"<<endl;
+    for(int i =0 ; i<n; i++){
+    	cin>>i;
+    	    }  
+    cout<<i; 
+    return 0;
+}
